e00eb47944077d634bc50486f705340c4d5f3e97
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-02-26 10:48:59 +0900

    push project 01

---
+++ project01/php-and-html.php
@@ -0,0 +1,13 @@
+<?php 
+    print("
+        <html>
+            <head>
+                <title>PHP and HTML</title>
+            </head>
+            <body>
+                <h1>Hello SAGU!</h1>
+                <p>We are learning PHP Codes.</p>
+            </body>
+        </html>        
+    "); 
+?>
