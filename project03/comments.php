9cfdfc1304692d336bb70164eb1e6821d2b6c7c4
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-02-26 11:32:41 +0900

    push project03

---
+++ project03/comments.php
@@ -0,0 +1,15 @@
+<?php 
+    // this is my first single line comment in php
+    echo "single line comment using '//' <br><br> ";
+
+    # this is also a 
+    # single line comment
+    echo "single line comment using '#' <br><br>";
+
+    /* 
+       this is 
+       multiple line comment 
+       in php 
+    */
+    echo "multiple line comment in php using '/*...*/' "
+?>
